d647605691c2272cdfb7fb8a0c88b3e4d0145729
Zach <zacharyamassey@gmail.com>
2025-09-18 18:35:24 -0400

    added example function call in tester.js

--- tester.js
+++ tester.js
@@ -1,4 +1,4 @@
-const funcStdoutFailure = (func, expout, args, description='', actual) => {
+const funcStdoutFailure = (func, expout, args, description = '', actual) => {
   try {
     console.error(`
 
@@ -15,9 +15,9 @@ const funcStdoutFailure = (func, expout, args, description='', actual) => {
   }
 };
 
-const stdOutSuccess = (func, expout, args, description='', actual) => {
-  try{
-  console.log(`
+const stdOutSuccess = (func, expout, args, description = '', actual) => {
+  try {
+    console.log(`
       TEST PASSED! FOR ${func.name}
       =====================================
       expected output: ${expout}
@@ -25,11 +25,11 @@ const stdOutSuccess = (func, expout, args, description='', actual) => {
       arguments passed to function:  ${args.join(', ')};
       function description: ${description || 'no description'}
       =====================================
-  `)
-  } catch(err){
+  `);
+  } catch (err) {
     console.error('Unable to standard output function success message::', err.message);
   }
-}
+};
 
 function functionTest(funcCallback, args, expout, description) {
   try {
@@ -37,12 +37,31 @@ function functionTest(funcCallback, args, expout, description) {
     if (result != expout) {
       funcStdoutFailure(funcCallback, expout, args, description, result);
       console.error('The functions result does not equal expected output');
-    }else{
+    } else {
       stdOutSuccess(funcCallback, expout, args, description, result);
     }
   } catch (err) {
     console.error('Unable to test function', err.message);
   }
 }
+
+/**
+ * Example function to be passed to function test
+ * that adds 2 numbers together
+
+const add = (n1, n2) => {
+  return n1 + n2;
+};
+
+// args for functionTest as follows
+// add = name of function 
+// [1, 3] = array of args for add 
+// 4 = expected result 
+// "function that.." = Description of function 
+
+functionTest(add, [1, 3], 4, "function that adds two nums");
+ 
+ *
+ */
 
 module.exports = functionTest;
